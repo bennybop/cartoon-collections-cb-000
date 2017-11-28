c9b12b89bf66eff45b8fadc260110583c4cf86c8
bennybop <bennybop74@yahoo.com>
2017-11-28 00:53:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with.index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
 end
 
 def find_the_cheese# code an argument here
