a82a238374d920baf2a5be1c7b6b26ddb7932ca1
bennybop <bennybop74@yahoo.com>
2017-11-28 00:59:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,15 +3,3 @@ def roll_call_dwarves(dwarves)
     puts "#{index}. #{dwarf}"
   end
 end
-
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |call| call.capitalize + "!" }
-end
-
-def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? { |call| call.length > 4 }
-end
-
-def find_the_cheese(cheese_types)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
